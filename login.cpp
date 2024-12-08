dda34f1ee616e22274fc99d34e29aa454276b365
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-08 18:13:18 +0530

    menu created for user choice input

---
+++ login.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char ch;
+    cout<<"\n1.Log IN\n2.Sign UP\n3.Forgot Password\n4.Skadoosh\n Enter your choice :"<<endl;
+    cin>>ch;
+    return 0;
+}
