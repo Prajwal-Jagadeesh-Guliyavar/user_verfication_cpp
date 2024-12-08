a7e3bb63a39e95ceaa3867cecf3f6a4576c01ea6
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-08 18:33:04 +0530

    class named temp is created with the function declarations of the actions mentioned in the menu

--- login.cpp
+++ login.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class temp{
+
+    string username,useremail,password;
+    fstream file;
+
+    public:
+        void logIN();
+        void signUP();
+        void forgotPassword();
+} obj;
+
 int main(){
 
-    char ch;
+    int ch;
     cout<<"\n1.Log IN\n2.Sign UP\n3.Forgot Password\n4.Skadoosh\n Enter your choice :"<<endl;
     cin>>ch;
+    switch(ch){
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            cout<<"Invalid choice.............!"<<endl; 
+    }
+
     return 0;
 }
